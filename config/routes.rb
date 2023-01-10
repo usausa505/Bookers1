d9d5df2a4edfc9cc925ee39e73c1408ff0d77120
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-10 14:10:54 +0000

    [Work interruption] 3-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+  get '/top' => 'homes#top'
 
 end
 
