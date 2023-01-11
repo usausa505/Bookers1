8a8bfe6194b78a95f1f5061ade84daaeea463753
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-11 09:54:33 +0000

    [Check point] 3-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/top' => 'homes#top'
-
+  get 'top' => 'homes#top'
+  resources :books
 end
 
