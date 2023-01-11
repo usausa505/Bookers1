b0b8d18e52e52e2548e023b7729f2f1dc902c66d
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-11 14:08:09 +0000

    [Work interruption] 3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book.id)
+    book = Book.new(book_params)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
     else
       render :new
     end
+
   end
 
   def index
@@ -40,5 +42,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
