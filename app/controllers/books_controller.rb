dd94aa090b89ae8b0f4b5e952000e97bd3c8a367
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-13 07:39:25 +0000

    [Check point] 3-13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
@@ -30,7 +30,12 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
